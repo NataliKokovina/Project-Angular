3fc8b88c20ab46daf38f8c2756dc7ed772db9bab
Nataliya Kokovina <kokovina.nat>
2019-03-17 19:42:41 +0200

    add number of people

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { MiniCalendarModule } from 'mini-calendar';
 import { ClickOutsideModule } from 'ng-click-outside';
-import { HomeComponent } from './home.component';
+
+// import { CityComponent } from 'src/app/city/city.component'
+// import { HomeComponent } from 'src/app/home/home.component';
+// import { HomeServiceService } from './home-service.service';
+// import { City } from '../home/home';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MiniCalendarModule,
-    ClickOutsideModule
+    ClickOutsideModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
+  providers: [],
   bootstrap: []
 })
 export class HomeModule { }
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,4 +1,18 @@
-export class Home {
-  
-}
+// export class City {
+//   name: string;
+
+
+//   constructor() {
+
+//   }
+
+// }
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// // import { City } from 'src/app/city/city.module';
+// import { enableProdMode } from '@angular/core';
+
+// enableProdMode();
+
+// platformBrowserDynamic().bootstrapModule(City)
+//   .catch(err => console.log(err));
 
